6c4d2befd49771eb2b397289a092999a573b1de3
MaciejJanowicz <163749512+MaciejJanowicz@users.noreply.github.com>
2024-06-30 09:29:25 +0200

    Students.java

--- Students.java
+++ Students.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Students {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Students {
+    private static List<Student> students = new ArrayList();
+
+    public Students() {
+    }
+
+    public static void add(Student student) {
+        students.add(student);
+    }
+
+    public static void promoteAll() {
+        Iterator var0 = students.iterator();
+
+        while(var0.hasNext()) {
+            Student student = (Student)var0.next();
+            student.promote();
+        }
+
+    }
+
+    public static void displayAll() {
+        Iterator var0 = students.iterator();
+
+        while(var0.hasNext()) {
+            Student student = (Student)var0.next();
+            System.out.println(student.getInfo());
+        }
+
+    }
 }
