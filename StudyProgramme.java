7158e49f2ee8dbd43285e21d52517f3ef2c7c380
MaciejJanowicz <163749512+MaciejJanowicz@users.noreply.github.com>
2024-06-30 09:29:54 +0200

    StudyProgramme.java

--- StudyProgramme.java
+++ StudyProgramme.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class StudyProgramme {
+public class StudyProgramme {
+    private String name;
+    private String description;
+    private int semesters;
+    private int maxItns;
+
+    public StudyProgramme(String name, String description, int semesters, int maxItns) {
+        this.name = name;
+        this.description = description;
+        this.semesters = semesters;
+        this.maxItns = maxItns;
+    }
+
+    public int getSemesters() {
+        return this.semesters;
+    }
+
+    public int getMaxItns() {
+        return this.maxItns;
+    }
 }
